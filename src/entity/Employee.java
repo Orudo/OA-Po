e9ae8869ec627cf60e9cc077590cf4ffa8273de6
orudo <1779181136@qq.com>
2017-05-15 13:29:35 +0800

    Employee op alpha1

--- src/entity/Employee.java
+++ src/entity/Employee.java
@@ -1,5 +1,14 @@
 package entity;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by martin on 17-4-24.
  */
@@ -23,6 +32,67 @@ public class Employee extends User {
     String email;
     String photoPath;
     private  Organization organization;
+    private static SessionFactory factory=new Configuration().configure("entity/hibernate.cfg.xml").buildSessionFactory();
+    public static void addEmployee(String userName,String passwd,String name,String department,String title,String phone,String  landlinePhone,String email,String photoPath){
+        Session session = factory.openSession();
+        Transaction t=session.beginTransaction();
+
+        Employee employee=new Employee();
+        employee.setUserName(userName);
+        employee.setPasswd(passwd);
+        employee.setName(name);
+        employee.setDepartment(department);
+        employee.setTitle(title);
+        employee.setPhone(phone);
+        employee.setLandlinePhone(landlinePhone);
+        employee.setEmail(email);
+        employee.setPhotoPath(photoPath);
+
+
+        session.persist(employee);
+        t.commit();
+        session.close();
+    }
+
+    public static Employee GetEmployeeById(String id)
+    {
+        Session session=factory.getCurrentSession();
+        Query query=session.createQuery("from User where id = '" +id+"'");
+
+        List<Employee> users=query.list();
+
+        Transaction t=session.beginTransaction();
+        t.commit();
+        if (users.iterator().hasNext())
+        {
+            return users.iterator().next();
+        }else return null;
+    }
+
+
+    public static void DelEmployeeById(String id)
+    {
+        Session session=factory.getCurrentSession();
+        Employee employee=session.load(Employee.class,id);
+        session.beginTransaction();
+        session.delete(employee);
+        session.getTransaction().commit();
+    }
+    public static void DelEmployeeByEntity(Employee employee)
+    {
+        Session session=factory.getCurrentSession();
+        session.beginTransaction();
+        session.delete(employee);
+        session.getTransaction().commit();
+    }
+
+    public static void UpdateEmployee(Employee employee)
+    {
+        Session session=factory.getCurrentSession();
+        session.beginTransaction();
+        session.update(employee);
+        session.getTransaction().commit();
+    }
 
     public Organization getOrganization() {
         return organization;
